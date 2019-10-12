4d1b7bffec38061d826b92a011ca7b8951d31455
geckse <mail@geckse.de>
2019-10-12 16:42:53 +0200

    mocked raw web section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,11 @@ import M from 'materialize-css'
 /*
   Init Materialize Modules
 */
-$(document).ready(function(){
-  M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.web-portfolio-slide');
+  var options = {
+    dist: 0,
+    padding: 30
+  };
+  var instances = M.Carousel.init(elems, options);
 });
